09100b934d174f685fbe6ffc2bba95a29c6ee82f
kush01012005 <85388374+kush01012005@users.noreply.github.com>
2024-01-10 07:08:13 -0500

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {
+      width:{
+        '10vw': '10vw', '20vw': '20vw', '33vw':'33vw', '45vw':'45vw', '95vw':'95vw',
+      }
     },
   },
 
